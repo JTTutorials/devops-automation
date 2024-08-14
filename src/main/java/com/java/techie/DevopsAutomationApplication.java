f283ed102346e0f819de7a9186a811d7933e23f4
arsule1985 <arsule1985@gmail.com>
2024-08-14 18:23:17 -0400

    added welcome service

--- src/main/java/com/java/techie/DevopsAutomationApplication.java
+++ src/main/java/com/java/techie/DevopsAutomationApplication.java
@@ -3,17 +3,20 @@ package com.java.techie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping
 public class DevopsAutomationApplication {
 
 	@GetMapping("/health")
 	public String healthCheck() {
 		return "Devops Automation Application is up and running!";
+	}
+	
+	@GetMapping
+	public String message() {
+		return "Welcome to Devops Automation Application!";
 	}
 	
 	public static void main(String[] args) {
