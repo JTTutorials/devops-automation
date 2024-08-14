7f37f724a16da1f95a82484f00783b6955c49268
arsule1985 <arsule1985@gmail.com>
2024-08-14 18:05:42 -0400

    health check service

--- src/main/java/com/java/techie/DevopsAutomationApplication.java
+++ src/main/java/com/java/techie/DevopsAutomationApplication.java
@@ -2,10 +2,18 @@ package com.java.techie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping
 public class DevopsAutomationApplication {
 
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "Devops Automation Application is up and running!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsAutomationApplication.class, args);
 	}
